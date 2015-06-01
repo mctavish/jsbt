7d312b966e15530b95bc81bfc628c6d1c228f3d9
James McTavish <jwmctavi@gmail.com>
2015-06-01 23:27:41 +0000

    Render a sample terrain object

--- scripts/ui.js
+++ scripts/ui.js
@@ -5,20 +5,35 @@ var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeig
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setClearColor( 0x6688ff, 1);
 document.getElementById('board').appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var holder = new THREE.Group();
+scene.add(holder);
 
-camera.position.z = 5;
+var light = new THREE.DirectionalLight(0xffffff, 0.8);
+light.position.set(3, -1, 2);
+scene.add(light);
+
+// instantiate a loader
+var loader = new THREE.ObjectLoader();
+
+// load a resource
+loader.load(
+	'models/terrain/clear.js',
+	function(object) {
+		holder.add(object);
+	}
+);
+
+camera.position.z = 30;
+camera.position.y = -20;
+camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 var render = function () {
 	requestAnimationFrame( render );
 
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;
+	holder.rotation.z += 0.01;
 
 	renderer.render(scene, camera);
 };
